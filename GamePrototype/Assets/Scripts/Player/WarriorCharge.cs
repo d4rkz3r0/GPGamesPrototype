74b518f086c4455b099fa9fde02cfb72c8a3b36e
LamentedHarp <longjof@woh.rr.com>
2016-05-04 14:33:40 -0400

    3 new scripts are added and the Player prefab has been updated to include a warrior charge collider.

---
+++ GamePrototype/Assets/Scripts/Player/WarriorCharge.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarriorCharge : AbilityScript {
+
+    public const float fowardVel = 20.0f;
+    public const float actionDuration = 1.0f;
+    public const float cooldownDuration = 1.5f;
+    public bool firstFrameActivation;
+
+    Quaternion targetRotation;
+    Rigidbody rBody;
+    CapsuleCollider capsuleCollider;
+    PlayerController playerController;
+    float actionTimer, cooldownTimer;
+    int inUse_ready_onCooldown;
+    /* inUse = 1, ready = 0, onCooldown = -1 */
+
+    public Quaternion TargetRotation
+    {
+        get { return targetRotation; }
+    }
+
+    void Start()
+    {
+        targetRotation = transform.rotation;
+
+        if (GetComponent<Rigidbody>())
+            rBody = GetComponent<Rigidbody>();
+        else
+            Debug.LogError("The player needs a rigid body.");
+
+        if (GameObject.FindGameObjectWithTag("WarriorChargeCollider"))
+        {
+            if (GameObject.FindGameObjectWithTag("WarriorChargeCollider").GetComponent<CapsuleCollider>())
+                capsuleCollider = GameObject.FindGameObjectWithTag("WarriorChargeCollider").GetComponent<CapsuleCollider>();
+            else
+                Debug.LogError("The object tagged WarriorChargeCollider needs a capsule collider.");
+        }
+        else
+            Debug.LogError("There is no object tagged WarriorChargeCollider");
+
+        if (GetComponent<PlayerController>())
+        {
+            playerController = GetComponent<PlayerController>();
+        }
+        else
+            Debug.LogError("The player controller script is not attached to this object.");
+
+
+        firstFrameActivation = false;
+        actionTimer = cooldownTimer = 0.0f;
+    }
+
+    void Update()
+    {
+        if (inUse_ready_onCooldown == 1)
+            UpdateActionTimer();
+        else if (inUse_ready_onCooldown == -1)
+            UpdateCooldownTimer();
+    }
+
+    void FixedUpdate()
+    {
+        Run();
+    }
+
+    void Run()
+    {
+        if (firstFrameActivation)
+            ActivateAction();
+        else if (inUse_ready_onCooldown == 1)
+        {
+            rBody.velocity = transform.forward * fowardVel;
+        }
+    }
+
+    void UpdateActionTimer()
+    {
+        if (actionTimer >= actionDuration)
+        {
+            actionTimer = 0.0f;
+            inUse_ready_onCooldown = -1;
+        }
+        else
+        {
+            actionTimer += Time.deltaTime;
+        }
+    }
+
+    void UpdateCooldownTimer()
+    {
+        if (cooldownTimer >= cooldownDuration)
+        {
+            cooldownTimer = 0.0f;
+            inUse_ready_onCooldown = 0;
+        }
+        else
+        {
+            cooldownTimer += Time.deltaTime;
+        }
+    }
+
+    void ActivateAction()
+    {
+        actionTimer = 0.0f;
+        inUse_ready_onCooldown = 1;
+        capsuleCollider.enabled = true;
+        playerController.enabled = false;
+    }
+}
---
+++ GamePrototype/Assets/Scripts/Player/WarriorChargeCollider.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarriorChargeCollider : MonoBehaviour {
+
+    CapsuleCollider playerCollider;
+    CapsuleCollider chargeCollider;
+    Rigidbody playerRigidBody;
+
+
+    void Start()
+    {
+        if (GameObject.FindGameObjectWithTag("Player"))
+        {
+            if (GameObject.FindGameObjectWithTag("Player").GetComponent<CapsuleCollider>())
+                playerCollider = GameObject.FindGameObjectWithTag("Player").GetComponent<CapsuleCollider>();
+            else
+                Debug.LogError("The player needs a capsule collider");
+        }
+        else
+            Debug.LogError("There needs to be an object tagged as player.");
+
+        if (GameObject.FindGameObjectWithTag("Player"))
+        {
+            if (GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>())
+                playerRigidBody = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>();
+            else
+                Debug.LogError("The player needs a rigid body.");
+        }
+        else
+            Debug.LogError("There needs to be an object tagged as player.");
+
+        if (GetComponent<CapsuleCollider>())
+        {
+            chargeCollider = GetComponent<CapsuleCollider>();
+        }
+        else
+            Debug.LogError("This object needs a capsule collider.");
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            playerCollider.enabled = false;
+            playerRigidBody.useGravity = false;
+        }
+        else if (other.CompareTag("Wall"))
+        {
+
+        }
+    }
+}
