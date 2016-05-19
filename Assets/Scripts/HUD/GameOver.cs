6d1667011638e4ebd3d8174d54d334adafff8773
Blake186 <blake186@fullsail.edu>
2016-05-19 08:01:38 -0400

    Fixed stuff with vendor

--- Assets/Scripts/HUD/GameOver.cs
+++ Assets/Scripts/HUD/GameOver.cs
@@ -4,10 +4,10 @@ using UnityEngine.SceneManagement;
 public class GameOver : MonoBehaviour
 {
 
-    public Canvas WinScreen;
+    public Canvas LoseScreen;
     void Start()
     {
-        WinScreen.enabled = false;
+        LoseScreen.enabled = false;
     }
 
     // Update is called once per frame
@@ -15,13 +15,13 @@ public class GameOver : MonoBehaviour
     {
         if (GetComponent<PlayerHealth>().CurHealth <= 0)
         {
-            WinScreen.enabled = true;
+            LoseScreen.enabled = true;
 
 
         }
         
 
-        if (Input.GetButton("StartButton") && WinScreen.enabled == true)
+        if (Input.GetButton("StartButton") && LoseScreen.enabled == true)
         {
             //Debug.Log(Input.GetButton("StartButton").ToString());
             SceneManager.LoadScene(0);
@@ -30,7 +30,7 @@ public class GameOver : MonoBehaviour
 
 
         //Debug.Log((Input.GetButton("SelectButton").ToString()));
-        if (Input.GetButton("SelectButton") && WinScreen.enabled == true)
+        if (Input.GetButton("SelectButton") && LoseScreen.enabled == true)
         {
             //UnityEditor.EditorApplication.isPlaying = false;
             Application.Quit();
