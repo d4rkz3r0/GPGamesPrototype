e609a6ff1f653223670334fb92927b5060b3d71c
LamentedHarp <longjof@woh.rr.com>
2016-05-20 10:05:13 -0400

    Level Remodeled

---
+++ Assets/RandomizeAnimSpeed.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomizeAnimSpeed : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Animator>().Play("Light Animation");
+        GetComponent<Animator>().speed = Random.Range(0.2f, 0.3f);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
